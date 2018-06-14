4b5dc44734ef40de40b844d30abdaa518108eb92
Steven Large <stevelarge7@gmail.com>
2018-06-14 12:19:44 -0700

    Added correlation function/generalized friction calculation script, June 14

--- ReadWrite.py
+++ ReadWrite.py
@@ -17,4 +17,23 @@ def WriteTrajectory(Path,Filename,Data):
 	file1.close()
 
 
+def WriteCorrelation(Path,Filename,CorrArray,LagTime):
+
+	CompleteName = os.path.join(Path,Filename)
+
+	file1 = open(CompleteName,'w')
+	for index in range(len(CorrArray)):
+		file1.write("%lf\t%lf\n" % (LagTime[index],CorrArray[index]))
+	file1.close()
+
+
+def WriteFriction(Path,Filename,GeneralizedFriction,Lambda):
+
+	CompleteName = os.path.join(Path,Filename)
+
+	file1 = open(CompleteName,'w')
+	file1.write('%lf\t%lf\n' % (Lambda,GeneralizedFriction))
+	file1.close()
+
+
 
